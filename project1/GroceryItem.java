98ad86946bbbbdbb2ac5eb2b5447c2b0eeefd6db
0dhapatel <39307304+0dhapatel@users.noreply.github.com>
2020-09-13 15:10:26 -0400

    Update GroceryItem.java

--- project1/GroceryItem.java
+++ project1/GroceryItem.java
@@ -2,7 +2,11 @@ public class GroceryItem {
   private String name;
   private double price;
   private boolean taxable;
-  ...
+  public GroceryItem(String name, double prize, boolean taxable){
+    this.name=name;
+    this.price=price;
+    this.taxable=taxable;
+  }
   public boolean equals(Object obj){ }
   public String toString() { }
   ...
