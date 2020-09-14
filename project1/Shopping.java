f689b1c9a4134beaa534b1969fcc2da2452e823e
jeimota <71230482+jeimota@users.noreply.github.com>
2020-09-14 19:08:18 -0400

    Update Shopping.java

--- project1/Shopping.java
+++ project1/Shopping.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Shopping {
-...
-  public void run() { }
-...
+
+	public void run() {
+		 
+		ShoppingBag bag = new ShoppingBag();
+		
+		Scanner keyboard = new Scanner(System.in); 
+		 char userImput = keyboard.next().charAt(0);
+		 
+		 System.out.println("Let's start shopping!"); 
+		 while(userImput != 'Q') {
+		 
+		 switch (userImput) {
+	          
+		  	  case 1:  userImput = 'A';
+		  	GroceryItem item = new GroceryItem(keyboard.nextLine(), keyboard.nextDouble(), keyboard.nextBoolean());
+		  	  bag.add(item);
+	                   break;
+	          case 2:  userImput = 'R';
+	
+	          bag.remove();
+	                   break;
+	          case 3:  userImput = 'P';
+	                   break;
+	          case 4:  userImput = 'C';
+	                   break;
+	          case 5:  userImput = 'Q';
+	                   break;
+	          default: System.out.println("Invalid command!"); 
+	                   break;
+      }
+		 
+		 }
+	}
 }
+
