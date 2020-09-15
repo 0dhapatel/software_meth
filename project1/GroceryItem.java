db55c5d37d78b2014f3d7770a9902d8695a89477
0dhapatel <39307304+0dhapatel@users.noreply.github.com>
2020-09-14 20:58:54 -0400

    Update GroceryItem.java

--- project1/GroceryItem.java
+++ project1/GroceryItem.java
@@ -2,11 +2,25 @@ public class GroceryItem {
   private String name;
   private double price;
   private boolean taxable;
+  
+  // Constructor
   public GroceryItem(String name, double prize, boolean taxable){
     this.name=name;
     this.price=price;
     this.taxable=taxable;
   }
+  
+  //Observer
+  public String get_name(){
+    return this.name;
+  }
+  public double get_price(){
+    return this.price;
+  }
+  public boolean taxable(){
+    return this.taxable;
+  }
+  
   public boolean equals(Object obj){
     if(this.name.equals(obj.name);
        if(this.price == obj.price && this.taxable == obj.taxable){
