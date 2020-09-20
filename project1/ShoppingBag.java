8a6af4711f30f45357c806eaeda2c9ab0d14e93f
0dhapatel <39307304+0dhapatel@users.noreply.github.com>
2020-09-20 17:34:56 -0400

    Update ShoppingBag.java

--- project1/ShoppingBag.java
+++ project1/ShoppingBag.java
@@ -5,19 +5,30 @@ public class ShoppingBag {
   private int size; // number of items currently in the bag
   private int capacity; // current capacity
   
-  public int DEFAULT_ITEM_SIZE = 5;
-  private static DecimalFormat df2 = new DecimalFormat("#.##");
+  public int DEFAULT_ITEM_SIZE = 5; // defult bag size 
+  private static DecimalFormat df2 = new DecimalFormat("#.##"); // formats double paramater to two decimal place
   
+  /**
+  * The public constructor method initiates bag, size, and capacity
+  * @param this.bag array of GroceryItem object 
+  * @param this.size integer of items in the bag
+  * @param this.capacity integer of items that can fit in the bag
+  */
   public ShoppingBag() {
     this.bag = new GroceryItem[DEFAULT_ITEM_SIZE];
     this.size = 0;
     this.capacity = 5;
   }
   
-  // helper method to find an item
+  
+  /**
+  * This is a helper method to find a first item in the bag
+  @param i integer pointer to help go through the bag
+  * @return -1 if item not found in the bag, else i if the array key is found
+  */
   private int find(GroceryItem item) {
   
-      for(int i = 0; i < this.size; i++ ) //the for loop runs through the array of items
+      for(int i = 0; i < this.size; i++ ) 
       {
           if(item.equals(this.bag[i])){
               return i; 
@@ -27,11 +38,12 @@ public class ShoppingBag {
       
    }
   
-  private void grow() { // helper method to grow the capacity
+  /**
+  * This is a helper method to grow the capacity of the bag by 5
+  @param i integer pointer to help go through the bag
+  */
+  private void grow() { 
     this.capacity = this.capacity + 5;
-    
-    //this.bag= Arrays.copyof(this.bag, this.capacity);
-    
     GroceryItem[] temp_bag = new GroceryItem[this.capacity];
     for (int i = 0; i < this.size; i++){
       temp_bag[i] = this.bag[i];
@@ -40,20 +52,28 @@ public class ShoppingBag {
     this.bag = temp_bag;
   } 
   
-  /* This methos adds item object into the bag array. 
-  The object consists of name, price, and taxable. 
-  At the end the @size is incremented by 1. */
-  
+  /**
+  * This methos adds item object into the bag array. 
+  * if the size is equal to capicity the bag has to grow().
+  * The object consists of name, price, and taxable. 
+  * At the end the size is incremented by 1. 
+  */
   public void add(GroceryItem item) {
   
       if(this.size==this.capacity){ //if capicity is smaller, use helper method grow to add more sapce 
-      grow();
+        grow();
       }
       
       this.bag[this.size] = item;
       this.size = this.size + 1; 
   }
   
+  /**
+  * This methos removes item object from the bag array. Finds the item from the bag using find(). Swaps the last item with the item to be removed.
+  * The last item is set as null. The object consists of name, price, and taxable. At the end the size is decremented by 1. 
+  * @param key helps to check if item is found
+  * @return true if the item was found and removed successfully, else false if item not found
+  */
   public boolean remove(GroceryItem item) {
       //key is to check in find() if item is in bag
       int key = find(item);
@@ -73,7 +93,11 @@ public class ShoppingBag {
       }
       return true;     
   }
-
+  
+  /**
+  * Calculates the total price that are in the bag without considering tax.
+  * @param salesPrice total price of the bag
+  */
   public double salesPrice(){
   
   double salesPrice;
@@ -87,6 +111,10 @@ public class ShoppingBag {
   
    }
   
+  /**
+  * Calculates the total tax that are in the bag.
+  * @param salesTax total tax of the bag
+  */
   public double salesTax() {
   
   double salesTax;
@@ -101,20 +129,14 @@ public class ShoppingBag {
       return salesTax; 
    }
   
+  /**
+  * This method helps to print out all of the items that are currently in the bag in a list format.
+  */
   public void print() {
-   /* String item_s = "";
-    if(this.size>1){
-      item_s=" items";
-    }else{
-      item_s=" item";
-    System.out.println("**You have "+ this.size + item_s +" in the bag.");
-    if(this.size < 1){
-      System.out.println("The bag is empty!");
-    } */
+   
     for(int i = 0; i < this.size; i++){
       System.out.println("·"+bag[i].toString());
   }
-    //System.out.println("**End of list");
    }
   
 }
