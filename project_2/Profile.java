7d1de6def50c58a3449ad79ec896bfd2dd465431
0dhapatel <39307304+0dhapatel@users.noreply.github.com>
2020-09-28 21:04:29 -0400

    Update Profile.java

--- project_2/Profile.java
+++ project_2/Profile.java
@@ -1,4 +1,17 @@
 public class Profile {
     private String fname;
     private String lname;
+    
+    public Profile(String fname, String lname){
+        this.fname = fname;
+        this.lname = lname;
+    }   
+        
+    public String get_fname(){
+        return this.fname;
+    }
+    
+    public String get_lname(){
+        return this.lname;
+    }
 }
