78f3d9d7fc4209af28a2b60d3cee2c6b3b78f787
0dhapatel <39307304+0dhapatel@users.noreply.github.com>
2020-09-28 21:43:52 -0400

    Update Date.java

--- project_2/Date.java
+++ project_2/Date.java
@@ -1,17 +1,31 @@
+import java.text.DecimalFormat;
+
 public class Date implements Comparable<Date> {
     private int year;
     private int month;
     private int day;
     
-    public Date date(int year, int month, int day){
+    private static DecimalFormat twoPlaces = new DecimalFormat("00");
+    private static DecimalFormat fourPlaces = new DecimalFormat("0000");
+    
+    
+    public Date(int year, int month, int day){
         this.year = year;
         this.month = month;
         this.day = day;
     }
     
+    public int compareTo(Date date) { //return 0, 1, or -1
+        return this.toString().equals(date.toString());
+    } 
     
+    public String toString() { //in the format mm/dd/yyyy
+        String dayFormat = twoPlaces.format(this.day);
+        String monthFormat = twoPlaces.format(this.month);
+        String yearFormat = fourPlaces.format(this.year);
+        String mergedString = monthFormat + "/" + dayFormat + "/" + yearFormat;
+        return mergedString;
+    } 
     
-    public int compareTo(Date date) { } //return 0, 1, or -1
-    public String toString() { } //in the format mm/dd/yyyy
     public boolean isValid() { }
 }
