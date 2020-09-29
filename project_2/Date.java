eb4bb6fce30a4ac1ff3ca4f9c1b8595c7466af8d
0dhapatel <39307304+0dhapatel@users.noreply.github.com>
2020-09-28 22:01:42 -0400

    Update Date.java

--- project_2/Date.java
+++ project_2/Date.java
@@ -1,12 +1,9 @@
-import java.text.DecimalFormat;
+
 
 public class Date implements Comparable<Date> {
     private int year;
     private int month;
     private int day;
-    
-    private static DecimalFormat twoPlaces = new DecimalFormat("00");
-    private static DecimalFormat fourPlaces = new DecimalFormat("0000");
     
     
     public Date(int year, int month, int day){
@@ -16,16 +13,57 @@ public class Date implements Comparable<Date> {
     }
     
     public int compareTo(Date date) { //return 0, 1, or -1
-        return this.toString().equals(date.toString());
+        if(this.year > date.year){
+            return 1;
+        }
+        if(this.year < date.year){
+            return -1;
+        }
+        if(this.month > date.month){
+            return 1;
+        }
+        if(this.month < date.month){
+            return -1;
+        }
+        if(this.day > date.day){
+            return 1;
+        }
+        if(this.day < date.day){
+            return -1;
+        }
+        return 0;
     } 
     
     public String toString() { //in the format mm/dd/yyyy
-        String dayFormat = twoPlaces.format(this.day);
-        String monthFormat = twoPlaces.format(this.month);
-        String yearFormat = fourPlaces.format(this.year);
-        String mergedString = monthFormat + "/" + dayFormat + "/" + yearFormat;
+        String mergedString = this.month + "/" + this.day + "/" + this.year;
         return mergedString;
     } 
     
-    public boolean isValid() { }
+    public boolean isValid() { 
+        if (this.month > 0 && this.month < 13){
+            if(this.month == 2){
+                if(this.year % 4 == 0){
+                    if(this.day > 0 && this.day 29){
+                        return true;
+                    }
+                }
+                else{
+                    if(this.day > 0 && this.day 28){
+                        return true;
+                    }
+                }
+            }
+            elseif(this.month == 4 && this.month == 6 && this.month == 9 && this.month == 11){
+                if(this.day > 0 && this.day 30){
+                    return true;
+                }
+            }
+            else{
+                if(this.day > 0 && this.day 31){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
