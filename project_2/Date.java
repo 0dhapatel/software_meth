0be5fd4250b9a1c3c8c1e4b783ecb0d17a0c4715
0dhapatel <39307304+0dhapatel@users.noreply.github.com>
2020-09-28 21:12:58 -0400

    Update Date.java

--- project_2/Date.java
+++ project_2/Date.java
@@ -2,6 +2,15 @@ public class Date implements Comparable<Date> {
     private int year;
     private int month;
     private int day;
+    
+    public Date date(int year, int month, int day){
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+    
+    
+    
     public int compareTo(Date date) { } //return 0, 1, or -1
     public String toString() { } //in the format mm/dd/yyyy
     public boolean isValid() { }
