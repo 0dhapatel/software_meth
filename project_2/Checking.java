c6d34fc14f40ee4ff572439b77ba7950218f0138
jeimota <71230482+jeimota@users.noreply.github.com>
2020-10-01 17:25:50 -0400

    Update Checking.java

--- project_2/Checking.java
+++ project_2/Checking.java
@@ -1,3 +1,32 @@
-public class Checking extends Account { 
-    private boolean directDeposit;
-}   
+public class Checking  extends Account  {
+	private boolean directDeposit; 
+	private final double annualInterestRate = 0.0005;
+	private int monthlyFee = 25; 
+	private final int waivedIfBalanceIs = 1500; 
+	
+	
+	public Checking(boolean directDeposit, Profile holder, double balance, Date dateOpen ) {
+		super(holder, balance, dateOpen);
+		this.directDeposit = directDeposit;
+		}
+	
+	
+	
+	public double monthlyInterest() {
+	return (super.getBalance() * annualInterestRate) / 12;
+		
+	}
+	
+	
+	public double monthlyFee(){
+		if(directDeposit == true || super.getBalance() == waivedIfBalanceIs ) {
+			monthlyFee = 0;
+		}
+		return monthlyFee;
+	
+	}
+
+			
+	}
+
+
