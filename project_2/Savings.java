bfcbc74396ae6e3d2a7922dc8dc4f460969c4706
jeimota <71230482+jeimota@users.noreply.github.com>
2020-10-01 17:30:36 -0400

    Update Savings.java

--- project_2/Savings.java
+++ project_2/Savings.java
@@ -1,7 +1,30 @@
-public class Savings extends Account {
-    private boolean isLoyal;
-    
-    
-    
-    
+public class Savings  extends Account{
+	private boolean isLoyal;
+	private double annualInterestRate = 0.0025;
+	private int monthlyFee = 5; 
+	private final int waivedIfBalanceIs = 300;
+	
+	public Savings(boolean isLoyal, Profile holder, double balance, Date dateOpen ) {
+		super(holder, balance, dateOpen);
+		this.isLoyal = isLoyal;
+		}
+
+	public double monthlyInterest() {
+		return (super.getBalance() * annualInterestRate) / 12;
+			
+		}
+		
+		
+
+	public double monthlyFee(){
+		if(isLoyal == true) {
+			annualInterestRate = 0.0035;
+		}
+		else if(super.getBalance() == waivedIfBalanceIs ) {
+			monthlyFee = 0;
+		}
+		return annualInterestRate;
+		
+	
+	}
 }
