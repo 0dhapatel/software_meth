2577a1f1253f6e15cb3d7189217df2de9a6b4b59
jeimota <71230482+jeimota@users.noreply.github.com>
2020-10-01 17:31:14 -0400

    Update MoneyMarket.java

--- project_2/MoneyMarket.java
+++ project_2/MoneyMarket.java
@@ -1,3 +1,26 @@
-public class MoneyMarket extends Account {
-    private int withdrawals;
+public class MoneyMarket extends Account{
+	private int withdrawals;
+	private double annualInterestRate = 0.0065;
+	private int monthlyFee = 12; 
+	private final int waivedIfBalanceIs = 2500;
+	
+	public MoneyMarket(int withdrawals, Profile holder, double balance, Date dateOpen ) {
+		super(holder, balance, dateOpen);
+		this.withdrawals = withdrawals;
+		}
+
+	public double monthlyInterest() {
+		return (super.getBalance() * annualInterestRate) / 12;
+			
+		}
+		
+		
+
+	public double monthlyFee(){
+		 if(super.getBalance() == waivedIfBalanceIs && withdrawals > 6 ) {
+			monthlyFee = 0;
+		}
+		 return monthlyFee;
+	
+	}
 }
