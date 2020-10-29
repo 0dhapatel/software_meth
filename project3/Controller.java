5ea4f162a3e790d81168ae5279d2699f9ce110d4
0dhapatel <39307304+0dhapatel@users.noreply.github.com>
2020-10-29 16:15:06 -0400

    Update Controller.java

--- project3/Controller.java
+++ project3/Controller.java
@@ -2,5 +2,24 @@
 package application;
 
 public class Controller {
-
-}
+  @FXML
+    void importFile(ActionEvent event) {
+    	FileChooser chooser = new FileChooser();
+		chooser.setTitle("Open Source File for the Import");
+		chooser.getExtensionFilters().addAll(new ExtensionFilter("Text Files", "*.txt"),
+				new ExtensionFilter("All Files", "*.*"));
+		Stage stage = new Stage();
+		File sourceFile = chooser.showOpenDialog(stage); //get the reference of the source file
+		//write code to read from the file.
+    }
+    
+    @FXML
+    void exportFile(ActionEvent event) {
+    	FileChooser chooser = new FileChooser();
+		chooser.setTitle("Open Target File for the Export");
+		chooser.getExtensionFilters().addAll(new ExtensionFilter("Text Files", "*.txt"),
+				new ExtensionFilter("All Files", "*.*"));
+		Stage stage = new Stage();
+		File targeFile = chooser.showSaveDialog(stage); //get the reference of the target file
+		//write code to write to the file.
+    }}
