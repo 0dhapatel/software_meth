fe2e36fc252356c1e8e99f929a0ac28bac73046f
0dhapatel <39307304+0dhapatel@users.noreply.github.com>
2020-11-11 14:55:26 -0500

    Create Extra.java

---
+++ Project #4/src/application/Extra.java
@@ -0,0 +1,14 @@
+package application;
+
+public class Extra {
+	
+	private String ingredient;
+	
+	public Extra (String ingredient) {
+		this.ingredient = ingredient;
+	}
+	
+	public String getIngredient () {
+		return this.ingredient;
+	}
+}
