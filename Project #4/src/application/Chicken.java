302935be1501be63e5a51f57b9664307d275df9b
0dhapatel <39307304+0dhapatel@users.noreply.github.com>
2020-11-11 14:52:22 -0500

    Update Chicken.java

--- Project #4/src/application/Chicken.java
+++ Project #4/src/application/Chicken.java
@@ -4,9 +4,34 @@
 package application;
 
 /**
- * @author Jei Mota
+ * @author Jei Mota and Dhaval Patel
  *
  */
 public class Chicken extends Sandwich {
+
+	private double prices = 8.99;
+	private String basicIngredients = "Fried Chicken,Spicy Sauce,Pickles";
+
+	@Override
+	public double price() {
+		return (prices + (extras.size()*PER_EXTRA));
+	}
+
+	@Override
+	public String basicIngredient() {
+		return basicIngredients;
+	}
+	
+	@Override
+	public boolean add(Object obj) {
+		super.add(obj);
+		return false;
+	}
+	
+	@Override
+	public boolean remove(Object obj) {
+		super.remove(obj);
+		return false;
+	}
 
 }
