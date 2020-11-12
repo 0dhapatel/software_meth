45006a69114f12bec0716e64610fb3226c763030
0dhapatel <39307304+0dhapatel@users.noreply.github.com>
2020-11-11 22:48:51 -0500

    Update SampleController.java

--- Project #4/src/application/SampleController.java
+++ Project #4/src/application/SampleController.java
@@ -1,16 +1,54 @@
 package application;
 
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class SampleController {
+import java.net.URL;
+import java.util.Arrays;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+public class SampleController implements Initializable{
+
+	public ComboBox<String> sandwiches;
+	public ListView<String> basic;
+	public ImageView imageChooser;
 	
-	public ComboBox comboBox;
+	ObservableList<String> type = FXCollections.observableArrayList("Chicken", "Beef", "Fish");
+	ObservableList<String> basicIngredients;
 	
-	
-	public void initialize() {
-	    comboBox.getItems().removeAll(comboBox.getItems());
-	    comboBox.getItems().addAll("Option A", "Option B", "Option C");
-	    comboBox.getSelectionModel().select("Option B");
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		sandwiches.setItems(type);
+		sandwiches.setValue("Chicken");
+		Sandwich defaultItem = new Chicken();
+		basicIngredients = FXCollections.observableArrayList(Arrays.asList(defaultItem.basicIngredient().split(",")));
+		basic.setItems(basicIngredients);
+		//Image image = new Image(path.toURI());
+		//imageChooser = new ImageView(image);
 	}
+	
+	public void comboClicked (ActionEvent event) {
+		Sandwich item;
+		switch(sandwiches.getValue()) {
+		case "Beef":
+			item = new Beef();
+			break;
+		case "Fish":
+			item = new Fish();
+			break;
+		default:
+			item = new Chicken();
+		}
+		
+		basicIngredients = FXCollections.observableArrayList(Arrays.asList(item.basicIngredient().split(",")));
+		basic.setItems(basicIngredients);
+	}
+	
 }
