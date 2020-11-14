5b26ded1183991076084c1f44f2d3d5ac908fc4b
0dhapatel <39307304+0dhapatel@users.noreply.github.com>
2020-11-14 13:10:08 -0500

    Update OrderDetailController.java

--- Project4/src/application/OrderDetailController.java
+++ Project4/src/application/OrderDetailController.java
@@ -4,8 +4,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.List;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -19,77 +19,88 @@ import javafx.stage.Stage;
 import javafx.stage.FileChooser.ExtensionFilter;
 
 public class OrderDetailController {
+	
+	private Order order = SampleController.passOrder();
+	
+	/**
+	 * Formats a double parameter to two decimal places of precision
+	 */
+	private static DecimalFormat df2 = new DecimalFormat("0.00");
 
     @FXML
-    private ListView<String> orderTextArea;
+    private ListView<String> orderList;
+    ObservableList<String> orderedSandwich;
 
     @FXML
-    private Button addButton;
+    private Button back;
 
     @FXML
-    private Button clearButton;
+    private TextField price;
 
-    @FXML
-    private Button removeButton;
-
-    @FXML
-    private Button backButton;
-
-    @FXML
-    private TextField priceTextField;
-
-    @FXML
-    private Button saveButton;
-
-    //Order order = new Order();
+    public void initialize() {
+    	orderedSandwich = FXCollections.observableArrayList(order.printOrder());
+    	orderList.setItems(orderedSandwich);
+    	price.setText("$"+df2.format(order.totalAmount()));
+	}
+    
     @FXML
     void addSameOrder(ActionEvent event) {
-    	
-    	
-    	
+    	if (orderList.getSelectionModel().getSelectedItem() == null) {
+    		return;
+    	}
+    	String orderedSandwich = orderList.getSelectionModel().getSelectedItem();
+    	int index = Character.getNumericValue(orderedSandwich.charAt(0));
+    	OrderLine sandwich = order.sameOrder(index-1);
+    	OrderLine copy = new OrderLine(sandwich.getSandwich());
+    	order.add(copy);
+    	initialize();
     }
 
     @FXML
     void clearOrder(ActionEvent event) {
-    	
-    	orderTextArea.getItems().clear();
-    	priceTextField.setText("0");
-
+    	//orderList.getItems().clear();
+    	//price.setText("$"+df2.format(0));
+    	order.clearOrder();
+    	initialize();
     }
 
     @FXML
     void removeOrder(ActionEvent event) {
-
+    	if (orderList.getSelectionModel().getSelectedItem() == null) {
+    		return;
+    	}
+    	String orderedSandwich = orderList.getSelectionModel().getSelectedItem();
+    	int index = Character.getNumericValue(orderedSandwich.charAt(0));
+    	OrderLine sandwich = order.sameOrder(index-1);
+    	order.remove(sandwich);
+    	initialize();
     }
 
     @FXML
     void returnOrderScreen(ActionEvent event) {
-
+    	back.getScene().getWindow().hide();
     }
     
     @FXML
     void saveOrder(ActionEvent event) throws IOException {
-    	
     	FileChooser chooser = new FileChooser();
-    	chooser.setTitle("Open Target File for the Export");
+		chooser.setTitle("Open Target File for the Export");
 		chooser.getExtensionFilters().addAll(new ExtensionFilter("Text Files", "*.txt"),
 				new ExtensionFilter("All Files", "*.*"));
 		Stage stage = new Stage();
-		
-		//get the reference of the target file
-		File targeFile = chooser.showSaveDialog(stage);
-			
-		BufferedWriter bf = new BufferedWriter(new FileWriter(targeFile));
-		//bf.write(order.printOrder());
-		bf.flush();
-		bf.close();
-
+		File targetFile = chooser.showSaveDialog(stage); //get the reference of the target file
+		//write code to write to the file.
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(targetFile));
+			ArrayList<String> print = order.printOrder();
+			for (int i = 0; i<print.size(); i++) {
+				writer.write(print.get(i)+"\n"); 
+			}
+			writer.close();
+			//output.appendText("Export Complete\n");
+		} catch (IOException e) {
+			//output.appendText("File cannot be written.\n");
+		}
     }
-    
-    public void setListView(ArrayList<String> orderLines) {
-    	ObservableList<String> orderLinesList = FXCollections.observableArrayList(orderLines);
-    	orderTextArea.setItems(orderLinesList);
-    }
-
 
 }
