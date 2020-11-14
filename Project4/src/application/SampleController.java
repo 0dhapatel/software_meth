61eafd1d253c24b8f6450a192ba7c8745c908934
0dhapatel <39307304+0dhapatel@users.noreply.github.com>
2020-11-14 13:10:34 -0500

    Update SampleController.java

--- Project4/src/application/SampleController.java
+++ Project4/src/application/SampleController.java
@@ -1,5 +1,7 @@
 package application;
 
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
@@ -7,29 +9,21 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.stage.FileChooser;
-import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.ResourceBundle;
-
 import java.text.DecimalFormat;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.fxml.Initializable;
+import javafx.fxml.FXMLLoader;
 
-public class SampleController implements Initializable{
+public class SampleController{
 	
-	private Order order;
+	private static Order order;
 	
 	/**
 	 * Formats a double parameter to two decimal places of precision
@@ -46,32 +40,23 @@ public class SampleController implements Initializable{
 	public TextArea output;
 	public Button addButton;
 	public TextField prices;
+	public Button orderShow;
 	
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
+	protected static Order passOrder() {
+		return order;
+	}
+	
+	public void initialize() {
 		sandwiches.setItems(type);
 		Sandwich ordered;
 		Image img;
-		if(Order.lineNumber == 0) {
+		if (Order.lineNumber == 0) {
 			order = new Order();
-			sandwiches.setValue("Chicken");
-			ordered = new Chicken();
-			img = new Image("Chicken.jpg");
-		} /*else {
-			addButton.setText("Update Order");
-			order = new Order();
-			sandwiches.setValue("Fish");
-			ordered = new Fish();
-			img = new Image("Fish.jpg");
-			Order.lineNumber = 0;
-		}*/ else {
-			Order.lineNumber = 0;
-			addButton.setText("Update Order");
-			OrderLine editOrder = order.startOrder();
-			ordered = editOrder.getSandwich();
-			sandwiches.setValue(ordered.type());
-			img = new Image(ordered.type()+".jpg");
 		}
+		sandwiches.setValue("Chicken");
+		ordered = new Chicken();
+		img = new Image("Chicken.jpg");
+		
 		basicIngredients = FXCollections.observableArrayList(Arrays.asList(ordered.basicIngredient().split(",")));
 		basic.setItems(basicIngredients);
 		imageChooser.setImage(img);
@@ -99,6 +84,17 @@ public class SampleController implements Initializable{
 		prices.setText("$"+df2.format(item.price()));
 	}
 	
+	public void clearList(ActionEvent event) {
+		if(order.lineNum() == 0) { 
+			 output.appendText("Nothing in Order\n"); 
+		} else {
+			ArrayList<String> print = order.printOrder(); 
+			for (int i = 0; i<print.size(); i++) {
+				output.appendText(print.get(i)+"\n"); 
+			}
+		}
+	}
+	
 	public void addOrder(ActionEvent event) {
 		Sandwich item;
 		switch(sandwiches.getValue()) {
@@ -111,44 +107,27 @@ public class SampleController implements Initializable{
 		default:
 			item = new Chicken();
 		}
-		OrderLine obj = new OrderLine(item, order.lineNum()+1);
+		OrderLine obj = new OrderLine(item);
 		if (order.add(obj)) {
 			output.appendText("Order Added\n");
 		} else {
 			output.appendText("Order Cannot Be Added\n");
 		}
+		initialize();
 	}
 	
 	public void showOrder(ActionEvent event) {
-		if(order.lineNum() == 0) { 
-			 output.appendText("Nothing in Order\n"); 
-		} else {
-			ArrayList<String> print = order.printOrder(); 
-			for (int i = 0; i<print.size(); i++) {
-				output.appendText(print.get(i)+"\n"); 
-			}
+		try {
+			Parent root = (BorderPane)FXMLLoader.load(getClass().getResource("OrderDetail.fxml"));
+			Scene scene = new Scene(root,700,700);
+			Stage stage = new Stage() ;
+			stage.setScene(scene);
+			stage.show();
+			stage.setTitle("Order Detail");
+			output.clear();
+			initialize();
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
 	}
-	
-	/*public void showOrder(ActionEvent event) {
-    	FileChooser chooser = new FileChooser();
-		chooser.setTitle("Open Target File for the Export");
-		chooser.getExtensionFilters().addAll(new ExtensionFilter("Text Files", "*.txt"),
-				new ExtensionFilter("All Files", "*.*"));
-		Stage stage = new Stage();
-		File targetFile = chooser.showSaveDialog(stage); //get the reference of the target file
-		//write code to write to the file.
-		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter(targetFile));
-			ArrayList<String> print = order.printOrder();
-			for (int i = 0; i<print.size(); i++) {
-				writer.write(print.get(i)+"\n"); 
-			}
-			writer.close();
-			output.appendText("Export Complete\n");
-		} catch (IOException e) {
-			output.appendText("File cannot be written.\n");
-		}
-    }*/
-	
 }
