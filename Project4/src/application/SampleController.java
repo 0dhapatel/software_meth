8f2cc4bdf9bee4a8783b945613f65586bee09b0e
jeimota <71230482+jeimota@users.noreply.github.com>
2020-11-16 00:40:49 -0500

    Update SampleController.java

--- Project4/src/application/SampleController.java
+++ Project4/src/application/SampleController.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -21,31 +22,40 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 
-public class SampleController{
-	
+public class SampleController {
+
 	private static Order order;
-	
+
 	/**
 	 * Formats a double parameter to two decimal places of precision
 	 */
 	private static DecimalFormat df2 = new DecimalFormat("0.00");
-	
+
+	final int MAX_INGREDIENTS = 6;
+
 	public ComboBox<String> sandwiches;
 	public ListView<String> basic;
+	public ListView<String> extra;
+	public ListView<String> extraToAdd;
 	public ImageView imageChooser;
-	
+
 	ObservableList<String> type = FXCollections.observableArrayList("Chicken", "Beef", "Fish");
 	ObservableList<String> basicIngredients;
-	
+	ObservableList<String> extras = FXCollections.observableArrayList("Tomatoes", "Lettuce", "Onions", "Olives",
+			"Ketchup", "Mustard", "Cheece", "Pickles", "Mayo", "Spinach");
+	ObservableList<String> extrasToAdd = FXCollections.observableArrayList();
+
 	public TextArea output;
 	public Button addButton;
 	public TextField prices;
 	public Button orderShow;
-	
+	public Button remove;
+	public Button clear;
+
 	protected static Order passOrder() {
 		return order;
 	}
-	
+
 	public void initialize() {
 		sandwiches.setItems(type);
 		Sandwich ordered;
@@ -56,17 +66,64 @@ public class SampleController{
 		sandwiches.setValue("Chicken");
 		ordered = new Chicken();
 		img = new Image("Chicken.jpg");
-		
+
 		basicIngredients = FXCollections.observableArrayList(Arrays.asList(ordered.basicIngredient().split(",")));
 		basic.setItems(basicIngredients);
 		imageChooser.setImage(img);
-		prices.setText("$"+df2.format(ordered.price()));
-		
+		prices.setText("$" + df2.format(ordered.price()));
+		extra.setItems(extras);
+		extraToAdd.setItems(extrasToAdd);
+		extra.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		extraToAdd.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
 	}
-	
-	public void comboClicked (ActionEvent event) {
+
+	public void addExtra(ActionEvent event) {
+		ObservableList<String> selectedItems = extra.getSelectionModel().getSelectedItems();
+		output.appendText(selectedItems.size() + "");
+		if (extrasToAdd.size() + selectedItems.size() <= MAX_INGREDIENTS) {
+			for (int i= 0; i < selectedItems.size(); i++ ) {
+				String ingredient = selectedItems.get(i);
+				output.appendText(ingredient + "\n");
+				extrasToAdd.add(ingredient);
+				int index = extras.indexOf(ingredient);
+				extras.remove(index);
+		}
+			extra.setItems(extras);
+			extraToAdd.setItems(extrasToAdd);
+			//extraToAdd.setItems(selectedItems);
+		}
+	}
+
+	public void removeExtra(ActionEvent event) {
+		ObservableList selectedIndices = extraToAdd.getSelectionModel().getSelectedIndices();
+
+		for (Object o : selectedIndices) {
+			String selectedItem = extraToAdd.getSelectionModel().getSelectedItem();
+			extrasToAdd.remove(selectedItem);
+			extras.add(selectedItem);
+
+		}
+	}
+
+	/**
+	 * public void extraIngredientList(Sandwich sandwich, String action) {
+	 * 
+	 * if(action.equals("add")) { for(extras.)) } }
+	 * 
+	 * public void addExtra (ActionEvent event) {
+	 * 
+	 * if(extras.size() < MAX_INGREDIENTS ) {
+	 * 
+	 * 
+	 * }
+	 * 
+	 * }
+	 **/
+
+	public void comboClicked(ActionEvent event) {
 		Sandwich item;
-		switch(sandwiches.getValue()) {
+		switch (sandwiches.getValue()) {
 		case "Beef":
 			item = new Beef();
 			break;
@@ -81,23 +138,23 @@ public class SampleController{
 		String imgName = sandwiches.getValue() + ".jpg";
 		Image img = new Image(imgName);
 		imageChooser.setImage(img);
-		prices.setText("$"+df2.format(item.price()));
+		prices.setText("$" + df2.format(item.price()));
 	}
-	
+
 	public void clearList(ActionEvent event) {
-		if(order.lineNum() == 0) { 
-			 output.appendText("Nothing in Order\n"); 
+		if (order.lineNum() == 0) {
+			output.appendText("Nothing in Order\n");
 		} else {
-			ArrayList<String> print = order.printOrder(); 
-			for (int i = 0; i<print.size(); i++) {
-				output.appendText(print.get(i)+"\n"); 
+			ArrayList<String> print = order.printOrder();
+			for (int i = 0; i < print.size(); i++) {
+				output.appendText(print.get(i) + "\n");
 			}
 		}
 	}
-	
+
 	public void addOrder(ActionEvent event) {
 		Sandwich item;
-		switch(sandwiches.getValue()) {
+		switch (sandwiches.getValue()) {
 		case "Beef":
 			item = new Beef();
 			break;
@@ -115,18 +172,18 @@ public class SampleController{
 		}
 		initialize();
 	}
-	
+
 	public void showOrder(ActionEvent event) {
 		try {
-			Parent root = (BorderPane)FXMLLoader.load(getClass().getResource("OrderDetail.fxml"));
-			Scene scene = new Scene(root,700,700);
-			Stage stage = new Stage() ;
+			Parent root = (BorderPane) FXMLLoader.load(getClass().getResource("OrderDetail.fxml"));
+			Scene scene = new Scene(root, 700, 700);
+			Stage stage = new Stage();
 			stage.setScene(scene);
 			stage.show();
 			stage.setTitle("Order Detail");
 			output.clear();
 			initialize();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
