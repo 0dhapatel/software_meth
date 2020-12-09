e8cc3110b65e8fea6e1b5cfadee4a91b28d27b63
jeimota <71230482+jeimota@users.noreply.github.com>
2020-12-08 23:11:29 -0500

    Update MainActivity.java

--- project5/MainActivity.java
+++ project5/MainActivity.java
@@ -4,28 +4,58 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    Button montclair;
-    Button Insectropolis;
-    Button Hunterdon;
-    Button Liberty;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        montclair = (Button) findViewById(R.id.Montclair);
-        montclair.setOnClickListener(new View.OnClickListener() {
+
+        final ListView lv = (ListView) findViewById(R.id.lv);
+        final Button btn = (Button) findViewById(R.id.btn);
+
+        String[] fruits = new String[] {
+                "Cape Gooseberry",
+                "Capuli cherry"
+        };
+        final List<String> fruits_list = new ArrayList<String>(Arrays.asList(fruits));
+
+        // Create an ArrayAdapter from List
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, fruits_list);
+
+        // DataBind ListView with items from ArrayAdapter
+        lv.setAdapter(arrayAdapter);
+
+        btn.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                
+            }
+
+            @Override
+            public void onClick(AdapterView<?> parent, View view,
+                                int position, long id) {
+                int itemPosition     = position;
+
+                // ListView Clicked item value
+                String  itemValue    = (String) List.getItemAtPosition(position);
                 Intent intent = new Intent(getApplicationContext(), Pricing.class);
                 startActivity(intent);
             }
         });
     }
 }
+
