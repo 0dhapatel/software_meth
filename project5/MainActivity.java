575953e32c2ae180656414ba69956a9ef0b9e428
0dhapatel <39307304+0dhapatel@users.noreply.github.com>
2020-12-09 00:16:36 -0500

    Update MainActivity.java

--- project5/MainActivity.java
+++ project5/MainActivity.java
@@ -1,61 +1,41 @@
-package com.example.museum;
+package com.example.museums;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.view.View.OnClickListener;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ListView lv = (ListView) findViewById(R.id.lv);
-        final Button btn = (Button) findViewById(R.id.btn);
-
-        String[] fruits = new String[] {
-                "Cape Gooseberry",
-                "Capuli cherry"
-        };
-        final List<String> fruits_list = new ArrayList<String>(Arrays.asList(fruits));
-
-        // Create an ArrayAdapter from List
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
-                (this, android.R.layout.simple_list_item_1, fruits_list);
-
-        // DataBind ListView with items from ArrayAdapter
-        lv.setAdapter(arrayAdapter);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-
+        ListView listView = (ListView) findViewById(R.id.listview);
+        final TextView textView = (TextView) findViewById(R.id.textview);
+        String[] players = new String[] {"CR7", "Messi", "Hazard", "Neymar"};
+        List<String> Players_list = new ArrayList<String>(Arrays.asList(players));
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Players_list);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                
-            }
-
-            @Override
-            public void onClick(AdapterView<?> parent, View view,
-                                int position, long id) {
-                int itemPosition     = position;
-
-                // ListView Clicked item value
-                String  itemValue    = (String) List.getItemAtPosition(position);
-                Intent intent = new Intent(getApplicationContext(), Pricing.class);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = (String) parent.getItemAtPosition(position);
+                //textView.setText("The best football player is : " + selectedItem);
+                Intent intent = new Intent(MainActivity.this, Price.class);
+                intent.putExtra("museum", selectedItem);
                 startActivity(intent);
             }
         });
     }
 }
-
