58b655c4ef95273872ba81da69df8f6138f00bf8
jeimota <71230482+jeimota@users.noreply.github.com>
2020-12-09 22:34:45 -0500

    Update MainActivity.java

--- project5/MainActivity.java
+++ project5/MainActivity.java
@@ -11,14 +11,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * This class controls activity_main.xml, and contains method onCreate.
+ *The porpose of this class is to provide and control the elements of the first museum activity.
+ *
+ * @author Jei Mota, Dhaval Patel
+ *
+ */
 public class MainActivity extends AppCompatActivity {
-
+    /**
+     * This method  provide and control the elements of the first museum activity.
+     * An ArrayList is used to hold the museum's names.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ListView listView = (ListView) findViewById(R.id.listview);
+        /**
+         * ArrayList is used to hold the museum's names.
+         */
         String[] museumName = new String[] {"Montclair Art Museum", "Insectropolis", "Hunterdon Art Museum", "Liberty Science Center"};
         List<String> museum_list = new ArrayList<>(Arrays.asList(museumName));
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, museum_list);
